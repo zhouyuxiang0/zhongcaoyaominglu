fea2a62d9736cb4d485eac75d798be579cbf9bf0
zhouyuxiang@epaynfc.com <zhouyuxiang@epaynfc.com>
2022-05-16 17:55:48 +0800

    chore

--- api/src/admin/admin.module.ts
+++ api/src/admin/admin.module.ts
@@ -1,11 +1,35 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminResolver } from './admin.resolver';
 import { AdminService } from './admin.service';
 import { Admin } from './entities/admin.entity';
+import { getRepository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Admin])],
   providers: [AdminResolver, AdminService],
 })
-export class AdminModule {}
+export class AdminModule implements OnModuleInit {
+  onModuleInit() {
+    const username = '18395433055';
+    const password = '929957640';
+    const adminRepo = getRepository(Admin);
+    adminRepo
+      .findOne({
+        where: {
+          username,
+        },
+      })
+      .then((admin) => {
+        if (admin) return;
+        const newAdmin = new Admin();
+        newAdmin.username = username;
+        newAdmin.salt = bcrypt.genSaltSync();
+        newAdmin.password = bcrypt.hashSync(password, newAdmin.salt);
+        newAdmin.save().then((v) => {
+          console.log(v);
+        });
+      });
+  }
+}
