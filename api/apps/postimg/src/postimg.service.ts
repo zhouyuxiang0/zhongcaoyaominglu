51cafadd2a353eecaf99773924f0513bce9b3f91
zhouyuxiang@epaynfc.com <zhouyuxiang@epaynfc.com>
2022-06-15 16:42:50 +0800

    chore

--- api/apps/postimg/src/postimg.service.ts
+++ api/apps/postimg/src/postimg.service.ts
@@ -1,16 +1,80 @@
 import { Injectable } from '@nestjs/common';
-import { cpus } from 'os';
-import { connect, launch } from 'puppeteer';
+import { connect, launch, Page, Browser } from 'puppeteer';
 
 export enum BrowserWsEndPointsStatus {
   free,
   process,
 }
 
+// TODO: 定时重启浏览器实例， 多线程，控制tab数量
 @Injectable()
 export class PostimgService {
   constructor() {
-    const num = cpus().length;
+    this.initBrowserWSEndPoint();
+  }
+  static browserIndex = 0;
+  static pageIndex = [];
+  private browserWsEndPoints: {
+    instance: string;
+    status: BrowserWsEndPointsStatus;
+  }[] = [];
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async test() {
+    const [browserIndex, browser] = await this.pickBrowser();
+    const [page] = await browser.pages();
+    await this.initPage(page);
+    await page.goto('https://postimages.org/');
+    const uploadBtn = await page.waitForSelector('.dz-hidden-input');
+    await uploadBtn.uploadFile('test.png');
+    page.once('load', () => {
+      page.screenshot({ path: 'shot.png' });
+    });
+  }
+
+  /**
+   * 挑选browser，目前为轮流选取
+   * @returns
+   */
+  async pickBrowser(): Promise<[number, Browser]> {
+    if (PostimgService.browserIndex >= this.browserWsEndPoints.length)
+      PostimgService.browserIndex = 0;
+    const pickedBrowser = this.browserWsEndPoints[PostimgService.browserIndex];
+    const browser = await connect({
+      browserWSEndpoint: pickedBrowser.instance,
+    });
+    const data: [number, Browser] = [PostimgService.browserIndex, browser];
+    PostimgService.browserIndex += 1;
+    return data;
+  }
+
+  async pickPage() {
+    //
+  }
+
+  /**
+   * 初始化page
+   */
+  async initPage(page: Page) {
+    await page.setRequestInterception(true);
+    page.on('request', (req) => {
+      if (['image', 'stylesheet', 'font'].includes(req.resourceType())) {
+        return req.abort();
+      }
+      return req.continue();
+    });
+  }
+
+  /**
+   * 初始化浏览器及WSEndPoint
+   */
+  async initBrowserWSEndPoint() {
+    // const num = cpus().length;
+    const num = 1;
+    PostimgService.pageIndex = new Array(num).fill(0);
     const p = [];
     for (let i = 0; i < num; i++) {
       p.push(
@@ -28,43 +92,10 @@ export class PostimgService {
         }),
       );
     }
-    Promise.all(p).then((v) => {
-      this.browserWsEndPoints = v.map((v) => ({
-        instance: v.wsEndpoint(),
-        status: BrowserWsEndPointsStatus.free,
-      }));
-      this.test();
-    });
-  }
-  browserWsEndPoints: {
-    instance: string;
-    status: BrowserWsEndPointsStatus;
-  }[] = [];
-  getHello(): string {
-    return 'Hello World!';
-  }
-
-  async test() {
-    const browserWSEndpoint = this.getBrowserWsEndPoints().instance;
-    console.log(browserWSEndpoint);
-
-    const browser = await connect({
-      browserWSEndpoint,
-    });
-    const page = await browser.newPage();
-    await page.goto('https://postimages.org/');
-    const uploadBtn = await page.waitForSelector('.dz-hidden-input');
-    await uploadBtn.uploadFile('test.png');
-    page.once('load', () => {
-      page.screenshot({ path: 'shot.png' });
-    });
-  }
-
-  getBrowserWsEndPoints() {
-    const free = this.browserWsEndPoints.find(
-      (v) => v.status == BrowserWsEndPointsStatus.free,
-    );
-    if (free) return free;
-    // TODO:
+    this.browserWsEndPoints = (await Promise.all(p)).map((v) => ({
+      instance: v.wsEndpoint(),
+      status: BrowserWsEndPointsStatus.free,
+    }));
+    this.test();
   }
 }
