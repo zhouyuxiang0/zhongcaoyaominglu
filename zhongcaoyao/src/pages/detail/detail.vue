3e22b14166b7b79ec269669b507e71e549848fb0
zhouyuxiang <zhouyuxiang@epaynfc.com>
2022-06-28 23:17:17 +0800

    chore

--- zhongcaoyao/src/pages/detail/detail.vue
+++ zhongcaoyao/src/pages/detail/detail.vue
@@ -4,6 +4,23 @@
       <text>中草药名录</text>
       <view class="title-bottom-img"></view>
     </view>
+    <view class="card-container">
+      <view class="card-tag">
+        <text>祛风湿散寒</text>
+      </view>
+      <view class="card">
+        <view class="child-card">
+          <view class="pinyin">
+            <text>mu gua</text>
+          </view>
+          <view class="hanzi">
+            <text>木 瓜</text>
+          </view>
+          <view class="img-container">
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
