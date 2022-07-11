46a4e9f05bfe520d0f8d12b3323d58d54a2b1fcb
zhouyuxiang@epaynfc.com <zhouyuxiang@epaynfc.com>
2022-07-11 15:04:07 +0800

    chore

--- api/apps/tts/src/tts.service.ts
+++ api/apps/tts/src/tts.service.ts
@@ -6,7 +6,7 @@ import { Response } from 'express';
 export class TtsService {
   async getHello(res: Response, msg: string) {
     // const buf = execSync(`paddlespeech tts --input ${msg} --output a.wav`);
-    const buf = execSync(`sudo paddlespeech -h`);
+    const buf = execSync(`/root/miniconda3/bin/paddlespeech -h`);
     return buf.toString();
     // const buf = await text2wav('你好,世界', { voice: 'zhy' });
     // const passThrough = new PassThrough();
