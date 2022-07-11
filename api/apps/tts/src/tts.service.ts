ae3a7e960ec5b94ac9ecc3c3d5bedfb88ddddc6a
zhouyuxiang@epaynfc.com <zhouyuxiang@epaynfc.com>
2022-07-11 15:00:57 +0800

    chore

--- api/apps/tts/src/tts.service.ts
+++ api/apps/tts/src/tts.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { exec, execFileSync, execSync } from 'child_process';
+import { execSync } from 'child_process';
 import { Response } from 'express';
-import { PassThrough, Readable } from 'stream';
 
 @Injectable()
 export class TtsService {
   async getHello(res: Response, msg: string) {
     // const buf = execSync(`paddlespeech tts --input ${msg} --output a.wav`);
-    const buf = execFileSync('/app/zhongcaoyaominglu/api/test');
+    const buf = execSync(`sudo paddlespeech -h`);
     return buf.toString();
     // const buf = await text2wav('你好,世界', { voice: 'zhy' });
     // const passThrough = new PassThrough();
