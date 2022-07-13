e4e864ae20e5415f52ae6af4572e3d72d5d10cd3
zhouyuxiang <zhouyuxiang@epaynfc.com>
2022-07-13 22:29:26 +0800

    chore

--- api/apps/tts/src/tts.service.ts
+++ api/apps/tts/src/tts.service.ts
@@ -7,12 +7,12 @@ import { createHash } from 'crypto';
 export class TtsService {
   private readonly logger = new Logger(TtsService.name);
   async getHello(res: Response, msg: string) {
-    const fileName = createHash('md5').update(msg).digest().toString();
+    const fileName = createHash('md5').update(msg).digest('hex');
     // const buf = execSync(`paddlespeech tts --input ${msg} --output a.wav`);
     execSync(
       `/root/miniconda3/bin/paddlespeech_client tts_online --server_ip 127.0.0.1 --port 8092 --protocol http --input ${msg} --output /app/audio/${fileName}.wav`,
     );
     this.logger.log(`生成/app/audio/${fileName}.wav`);
-    return res.sendFile(`/audio/${fileName}.wav`);
+    return res.sendFile(`/app/audio/${fileName}.wav`);
   }
 }
