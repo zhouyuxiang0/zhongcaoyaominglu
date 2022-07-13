6ea6cf5c079e61876071b6f936351711d81eca4f
zhouyuxiang <zhouyuxiang@epaynfc.com>
2022-07-13 22:17:07 +0800

    chore

--- api/apps/tts/src/tts.service.ts
+++ api/apps/tts/src/tts.service.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { execSync } from 'child_process';
 import { Response } from 'express';
 import { createHash } from 'crypto';
 
 @Injectable()
 export class TtsService {
+  private readonly logger = new Logger(TtsService.name);
   async getHello(res: Response, msg: string) {
     const fileName = createHash('md5').update(msg).digest().toString();
     // const buf = execSync(`paddlespeech tts --input ${msg} --output a.wav`);
     execSync(
-      `/root/miniconda3/bin/paddlespeech_client tts_online --server_ip 127.0.0.1 --port 8092 --protocol http --input ${msg} --output /audio/${fileName}.wav`,
+      `/root/miniconda3/bin/paddlespeech_client tts_online --server_ip 127.0.0.1 --port 8092 --protocol http --input ${msg} --output /app/audio/${fileName}.wav`,
     );
+    this.logger.log(`生成/app/audio/${fileName}.wav`);
     return res.sendFile(`/audio/${fileName}.wav`);
   }
 }
